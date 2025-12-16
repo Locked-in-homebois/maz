f835a0d2137bfd9fcfb4c5e04ad42a68c856e896
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-17 00:18:54 +0300

    small changes

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -17,32 +17,32 @@ const Hero = () => {
                 <div className="absolute inset-0 bg-black/40" />
                 {/* Text on top */}
                 <div className="relative z-10 h-full flex flex-col justify-center gap-4 px-8 max-w-4xl">
-                    <h1 className="text-7xl font-extrabold tracking-tight leading-tight text-white">
+                        <h1 className="text-7xl font-extrabold tracking-tight leading-tight text-white">
                         Precision Engineering with a Personal Touch
-                    </h1>
+                        </h1>
                     
-                    <div className="">
-                        <Link
-                        href="/"
-                        className="md:text-[21px] bg-amber-950 rounded-lg  text-[15px]  text-neutral-100 py-2 md:px-4.25 px-3.75 hover:bg-lime-900 transition md:font-medium font-[550]">
-                        View Portfolio
-                        </Link>
-                        <Link 
-                        href="/"
-                        className="md:text-[21px] bg-neutral-50 rounded-lg mx-3 text-[15px]  text-black py-2 md:px-4.25 px-3.75 hover:bg-lime-900 transition md:font-medium font-[550]">
-                            Contact Me 
-                        </Link>
-                    </div>
-                    <div className="translate-y-57">
+                        <div className="flex">
+                            <Link
+                            href="/"
+                            className="md:text-[21px] bg-amber-950 rounded-lg  text-[15px]  text-neutral-100 py-2 md:px-4.25 px-3.75 hover:bg-lime-900 transition md:font-medium font-[550]">
+                            View Portfolio
+                            </Link>
+                            <Link
+                            href="/"
+                            className="md:text-[21px] bg-neutral-50 rounded-lg mx-3 text-[15px]  text-black py-2 md:px-4.25 px-3.75 hover:bg-lime-900 transition md:font-medium font-[550]">
+                                Contact Me
+                            </Link>
+                        </div>
+                        <div className="translate-y-57">
                             <p className="text-2xl text-gray-200 max-w-2xl font-light leading-relaxed">
-                                King Faisal University-alhafuf
+                                King Faisal University -alhufuf
                             </p>
                             <p className="text-md text-gray-300 max-w-2xl font-light ">
                                 design supply and installation
                             </p>
+                        </div>
                     </div>
                 </div>
-            </div>
         </main>
     );
 };
