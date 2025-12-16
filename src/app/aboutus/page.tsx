d2be6b5a25f29179142af087857fb1c7ff8b3755
MNS <parsamanes@yahoo.com>
2025-12-16 18:25:40 +0300

    added about-us hero

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/src/components/layout/aboutus/hero";
+import React from "react";
+
+export default function page() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
