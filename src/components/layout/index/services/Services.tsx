8bbd41f126236324576ff5340ce01e6810a64cbb
MNS <parsamanes@yahoo.com>
2025-12-18 20:01:32 +0300

    services is done

--- src/components/layout/index/services/Services.tsx
+++ src/components/layout/index/services/Services.tsx
@@ -3,44 +3,46 @@ import { BOXCONTENT, HEADERCONTENT } from "./constants";
 import Image from "next/image";
 
 const Services = () => {
-    return (
-        <MaxWidthWrapper>
-            <div className="w-full grid gap-9 grid-cols-4 relative -z-30 justify-items-center ">
-                <div className="col-span-4 text-center shadow-black  w-full shadow- rounded-2xl">
-                    <h1 className="uppercase text-4xl font-black">
-                        {HEADERCONTENT.title}
-                    </h1>
-                    <p>{HEADERCONTENT.description}</p>
-                </div>
+	return (
+		<MaxWidthWrapper>
+			<div className="w-full grid gap-x-5 gap-y-2  grid-cols-3 relative -z-30 justify-items-center rounded-2xl my-20 ">
+				<div className="col-span-full text-center shadow-black  w-full shadow- rounded-2xl">
+					<h1 className="uppercase text-4xl font-black">
+						{HEADERCONTENT.title}
+					</h1>
+					<p>{HEADERCONTENT.description}</p>
+				</div>
 
-                {BOXCONTENT.map((item, index) => (
-                    <div
-                        key={index}
-                        className="grid gap-3 rounded-3xl  bg-black/10"
-                    >
-                        <div className=" w-full relative rounded-2xl -z-10">
-                            <Image
-                                src={item.image.src}
-                                alt={item.image.alt}
-                                width={1200}
-                                height={1000}
-                                className="rounded-[20px]"
-                            />
-                            <item.icon
-                                size={45}
-                                className="bg-neutral-50 absolute rounded-xl top-5 right-5 p-2 justify-self-end "
-                            />
-                        </div>
-                        <div className="text-center px-3 rounded-2xl flex flex-col justify-center">
-                            <h1 className="uppercase text-2xl font-black">
-                                {item.title}
-                            </h1>
-                            <p className="">{item.description}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </MaxWidthWrapper>
-    );
+				{BOXCONTENT.map((item, index) => (
+					<div
+						key={index}
+						className={`grid gap-3 rounded-3xl  ${item.colspan}`}
+					>
+						<div className=" w-full relative rounded-2xl max-h-[250px] overflow-hidden z-10">
+							<Image
+								src={item.image.src}
+								alt={item.image.alt}
+								width={1200}
+								height={1000}
+								className="rounded-[20px] "
+							/>
+							<item.icon
+								size={45}
+								className="bg-neutral-50 absolute rounded-xl top-5 right-5 p-2 justify-self-end "
+							/>
+						</div>
+						<div className="px-3 rounded-2xl flex flex-col justify-center">
+							<h1 className="uppercase text-2xl font-black">
+								{item.title}
+							</h1>
+							<p className="text-gray-500 font-light">
+								{item.description}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</MaxWidthWrapper>
+	);
 };
 export default Services;
--- src/components/layout/index/services/constants.ts
+++ src/components/layout/index/services/constants.ts
@@ -1,32 +1,36 @@
 import { ServiceBox, Header } from "./types";
 import { Hammer, SwatchBook } from "lucide-react";
 export const HEADERCONTENT: Header = {
-    title: "helloooooooo",
-    description: "aubdkaubksjdfbnwkuerufbcskjdbcfiuskdjbc",
+	title: "helloooooooo",
+	description: "aubdkaubksjdfbnwkuerufbcskjdbcfiuskdjbc",
 };
 export const BOXCONTENT: ServiceBox[] = [
-    {
-        title: "Hardscape",
-        description: "sdcaca sasdakj sbcuiab dcadc",
-        image: { src: "/image2.jpeg", alt: "image" },
-        icon: Hammer,
-    },
-    {
-        title: "Villas",
-        description: "skjcf baskjds bfakjs bdfckaj lbsd",
-        image: { src: "/image2.jpeg", alt: "image" },
-        icon: SwatchBook,
-    },
-    {
-        title: "facedes",
-        description: "asljdf nalscasdasd fasfd skhfbskd",
-        image: { src: "/image2.jpeg", alt: "image" },
-        icon: Hammer,
-    },
-    {
-        title: "Rock",
-        description: "asljdf nalscasdasd fasfd skhfbskd",
-        image: { src: "/image2.jpeg", alt: "image" },
-        icon: Hammer,
-    },
+	{
+		title: "Hardscape",
+		description: "sdcaca sasdakj sbcuiab dcadc",
+		image: { src: "/image2.jpeg", alt: "image" },
+		icon: Hammer,
+		colspan: "col-span-2",
+	},
+	{
+		title: "Villas",
+		description: "skjcf baskjds bfakjs bdfckaj lbsd",
+		image: { src: "/image2.jpeg", alt: "image" },
+		icon: SwatchBook,
+		colspan: "col-span-1",
+	},
+	{
+		title: "facedes",
+		description: "asljdf nalscasdasd fasfd skhfbskd",
+		image: { src: "/image2.jpeg", alt: "image" },
+		icon: Hammer,
+		colspan: "col-span-1",
+	},
+	{
+		title: "Rock",
+		description: "asljdf nalscasdasd fasfd skhfbskd",
+		image: { src: "/image2.jpeg", alt: "image" },
+		icon: Hammer,
+		colspan: "col-span-2",
+	},
 ];
