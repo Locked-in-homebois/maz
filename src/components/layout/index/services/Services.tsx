293e2189db7cae0415144bd56ffcd6a19629181c
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-18 17:47:51 +0300

    its ass but its a starting point

--- src/components/layout/index/services/Services.tsx
+++ src/components/layout/index/services/Services.tsx
@@ -1,16 +1,38 @@
 import MaxWidthWrapper from "@/src/components/ui/MaxWidthWrapper";
 import { BOXCONTENT, HEADERCONTENT } from "./constants";
+import Image from "next/image";
+
 const Services = () => {
     return (
         <MaxWidthWrapper>
             <div className="w-full grid gap-9 grid-cols-3 justify-items-center h-60">
-                <div className="bg-neutral-200  ">
+                <div className="col-span-3 text-center">
                     <h1>{HEADERCONTENT.title}</h1>
+                    <p>{HEADERCONTENT.description}</p>
                 </div>
 
-                <div className="bg-neutral-200 rounded-2xl ">hi</div>
-
-                <div className="bg-neutral-200 rounded-2xl ">hi</div>
+                {BOXCONTENT.map((item, index) => (
+                    <div key={index}>
+                        <div className="relative -z-10">
+                            <Image
+                                src={item.image.src}
+                                alt={item.image.alt}
+                                width={1200}
+                                height={1000}
+                                className="rounded-2xl absolute"
+                            />
+                            <item.icon className="bg-neutral-50 rounded-md relative justify-self-end size-9 " />
+                        </div>
+                        <div className="">
+                            <h1 className="backdrop-blur-2xl max-w-fit">
+                                {item.title}
+                            </h1>
+                            <p className="backdrop-blur-2xl max-w-fit">
+                                {item.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </MaxWidthWrapper>
     );
--- src/components/layout/index/services/constants.ts
+++ src/components/layout/index/services/constants.ts
@@ -1,12 +1,26 @@
-import { ServiceBox,Header } from "./types";
-import { Hammer, } from "lucide-react";
-export const HEADERCONTENT:Header={
-    title:"helloooooooo",
-    description:"aubdkaubksjdfbnwkuerufbcskjdbcfiuskdjbc"
+import { ServiceBox, Header } from "./types";
+import { Hammer, SwatchBook } from "lucide-react";
+export const HEADERCONTENT: Header = {
+    title: "helloooooooo",
+    description: "aubdkaubksjdfbnwkuerufbcskjdbcfiuskdjbc",
 };
-export const BOXCONTENT:ServiceBox={
-title:"",
-description:"",
-image:{src:"",alt:""},
-icon:Hammer
-}
+export const BOXCONTENT: ServiceBox[] = [
+    {
+        title: "aysdvgfc",
+        description: "sdcacasasdakjsbcuiabdcadc",
+        image: { src: "/image2.jpeg", alt: "image" },
+        icon: Hammer,
+    },
+    {
+        title: "ksudbvcksjbdv",
+        description: "skjcfbaskjdsbfakjsbdfckajlbsd",
+        image: { src: "/image2.jpeg", alt: "image" },
+        icon: SwatchBook,
+    },
+    {
+        title: "ksudahfkuhsf",
+        description: "asljdfnalscasdasdfasfdskhfbskd",
+        image: { src: "/image2.jpeg", alt: "image" },
+        icon: Hammer,
+    },
+];
