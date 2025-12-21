affdb36d110795e39dd8ae8b09b05de5d84fc340
MNS <parsamanes@yahoo.com>
2025-12-22 01:08:59 +0300

    fixed the rendering issue

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
+import OriginalFunction from "@/src/components/layout/projects/kemaloriginal/Original";
 import ProjectsGallery from "@/src/components/layout/projects/projects/Projects";
 
 export default function Home() {
 	return (
 		<main>
 			<ProjectsGallery />
+			<OriginalFunction />
 		</main>
 	);
 }
--- src/components/layout/projects/kemaloriginal/Original.tsx
+++ src/components/layout/projects/kemaloriginal/Original.tsx
@@ -1,55 +1,39 @@
 import MaxWidthWrapper from "@/src/components/ui/MaxWidthWrapper";
-import { LeftLowCard, TextPart, RightCards, } from "./types";
+import { LeftLowCard, TextPart, RightCards } from "./types";
 import { LeftLowCards_DATA, TextPart_DATA } from "./constants";
 import { Diamond, icons } from "lucide-react";
-import { title } from "process";
-
 
 export default function OriginalFunction() {
-    return (
+	return (
+		<MaxWidthWrapper className="">
+			<main className="grid grid-cols-2">
+				<div className="flex flex-col">
+					<Diamond />
+					{TextPart_DATA.iconTitle}
 
-        <MaxWidthWrapper className="">
-            <main className="grid grid-cols-2">
+					<div className="flex flex-col text-3xl font-bold gap-3">
+						{TextPart_DATA.header}
+					</div>
 
-                <div className="flex flex-col">
-                    <Diamond />
-                    {TextPart_DATA.iconTitle}
+					<div className="flex flex-col text-xl font-bold gap-3">
+						{TextPart_DATA.desc}
+					</div>
 
-                    <div className="flex flex-col text-3xl font-bold gap-3">
-                        {TextPart_DATA.header}
-                    </div>
+					<div className="grid grid-cols-2 gap-4 ">
+						{LeftLowCards_DATA.map((cards, index) => {
+							const IconComponent = cards.icon;
 
-                    <div className="flex flex-col text-xl font-bold gap-3">
-                        {TextPart_DATA.desc}
-                    </div>
-
-                    <div className="grid grid-cols-2 gap-4 ">
-                        {LeftLowCards_DATA.map((cards, index) =>
-                            <div
-                                key={index}
-                                icon={Diamond}
-                                title={cards.title}
-                                desc={cards.desc}
-
-                                div />
-
-
-                        )}
-
-                    </div>
-
-                </div>
-
-                <div>
-
-
-                </div>
-
-
-            </main>
-        </MaxWidthWrapper>
-
-    )
-
-
+							return (
+								<div key={index}>
+									<IconComponent className="" />
+									<h1>{cards.title}</h1>
+									<p>{cards.desc}</p>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</main>
+		</MaxWidthWrapper>
+	);
 }
