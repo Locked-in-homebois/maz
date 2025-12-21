a9f738b345d755946a0b2737d6e7739f32ff2285
MNS <parsamanes@yahoo.com>
2025-12-21 18:27:18 +0300

    fixed the masterpiece import on the main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 import Hero from "../components/layout/index/hero/hero";
 import Services from "../components/layout/index/services/Services";
-import Expertise from "../components/layout/index/expertise/Expertise";
-import MasterPiece from "../components/layout/index/maasterpiece/MasterPiece";
+import Expertise from "../components/layout/index/Expertise/Expertise";
+import MasterPiece from "../components/layout/index/masterpiece/MasterPiece";
 import ProjectsGallery from "../components/layout/projects/projects/Projects";
 
 export default function Home() {
-    return (
-        <main>
-            <Hero />
-            <Services />
-            <Expertise />
-            <MasterPiece />
-            <ProjectsGallery />
-        </main>
-    );
+	return (
+		<main>
+			<Hero />
+			<Services />
+			<Expertise />
+			<MasterPiece />
+			<ProjectsGallery />
+		</main>
+	);
 }
