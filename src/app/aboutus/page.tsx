f9f82a38de18b92223fb75d4184956173669664f
Parsa <parsaxavier@gmail.com>
2025-12-22 15:19:20 +0300

    pre prod

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,16 +1,13 @@
 import Hero from "@/src/components/layout/aboutus/hero/hero";
 import Phy from "@/src/components/layout/aboutus/phy/phy";
 import ProfileSection from "@/src/components/layout/aboutus/Profile/Skills";
-import Kemal from "@/src/components/layout/projects/kemal/Kemal";
-import React from "react";
 
 export default function page() {
-    return (
-        <main className="grid gap-10 mt-5">
-            <Hero />
-            <Phy />
-            <ProfileSection />
-            <Kemal />
-        </main>
-    );
+	return (
+		<main className="grid gap-10 mt-5">
+			<Hero />
+			<Phy />
+			<ProfileSection />
+		</main>
+	);
 }
