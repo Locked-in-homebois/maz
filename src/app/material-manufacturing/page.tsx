aaac4b56e5d06c54f938b5d31183159a48f776e2
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-24 21:26:21 +0300

    fix 1

--- src/app/material-manufacturing/page.tsx
+++ src/app/material-manufacturing/page.tsx
@@ -1,11 +1,9 @@
-import Carousel from "@/src/components/layout/material/Carousel/Carousel";
 import OriginalFunction from "@/src/components/layout/projects/kemaloriginal/Original";
 
 export default function Home() {
     return (
         <main>
             <OriginalFunction />
-            <Carousel />
         </main>
     );
 }
