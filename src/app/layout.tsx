7e96a6b012be60549dd8628912167b4a543beaa9
Parsa <parsaxavier@gmail.com>
2025-12-24 14:00:17 +0300

    footer: fixes names, dependancies and added a getDateFullYear for all rights reserved section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/layout/navbar";
-import Footer from "../components/ui/footer/footerthingy";
+import Footer from "../components/ui/footer/Footer";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
