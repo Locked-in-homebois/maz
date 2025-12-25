33a01b720af9ca32a424aac82e494675e6a22526
Majed Ahdab <majedahdab@192.168.1.103>
2025-12-25 22:03:39 +0300

    images and optimization

--- src/components/layout/aboutus/hero/types.ts
+++ src/components/layout/aboutus/hero/types.ts
@@ -1,14 +1,14 @@
 // src/components/sections/Hero/types.ts
 
 export interface HeroButton {
-	label: string;
-	href: string;
+    label: string;
+    href: string;
 }
 
 export interface HeroData {
-	header: string;
-	subheader: string;
-	imageSrc: string;
-	imageAlt: string;
-	buttons: HeroButton[];
+    header: string;
+    subheader: string;
+    image: { src: string; alt: string };
+
+    buttons: HeroButton[];
 }
