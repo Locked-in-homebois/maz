f7ee09fbfbfa004ca4c5380f529f31e0d0de882a
Parsa <parsaxavier@gmail.com>
2025-12-26 16:05:42 +0300

    hero optimization

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,27 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, IBM_Plex_Sans_Arabic } from "next/font/google";
+import { Geist, IBM_Plex_Sans_Arabic } from "next/font/google";
 import "../globals.css";
 import Navbar from "@/src/components/layout/navbar";
 import Footer from "@/src/components/ui/footer/Footer";
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {routing} from '@/src/i18n/routing';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/src/i18n/routing";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
-
 const ibmPlexSansArabic = IBM_Plex_Sans_Arabic({
-    variable: "--font-ibm-plex-sans-arabic",
-    subsets: ["arabic"],
-    weight: ["100", "200", "300", "400", "500", "600", "700"],
+	variable: "--font-ibm-plex-sans-arabic",
+	subsets: ["arabic"],
+	weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -31,12 +26,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
 	children,
-	params
+	params,
 }: {
 	children: React.ReactNode;
-	params: Promise<{locale: string}>;
+	params: Promise<{ locale: string }>;
 }) {
-	const {locale} = await params;
+	const { locale } = await params;
 
 	// Ensure that the incoming `locale` is valid
 	if (!routing.locales.includes(locale as any)) {
@@ -44,15 +39,14 @@ export default async function RootLayout({
 	}
 
 	const messages = await getMessages();
-    
-    // Select the primary font variable based on locale
-    const fontVariable = locale === 'ar' ? ibmPlexSansArabic.variable : geistSans.variable;
+
+	// Select the primary font variable based on locale
+	const fontVariable =
+		locale === "ar" ? ibmPlexSansArabic.variable : geistSans.variable;
 
 	return (
-		<html lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
-			<body
-				className={`${fontVariable} ${geistMono.variable} antialiased`}
-			>
+		<html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+			<body className={`${fontVariable}  antialiased`}>
 				<NextIntlClientProvider messages={messages}>
 					<Navbar />
 					{children}
