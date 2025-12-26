be965a1058ceaf6693aa37d35134f7c470bec4b3
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-26 16:09:03 +0300

    map fixed now loads when on screen only

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "../MaxWidthWrapper";
 import { LEFT_DATA, RIGHT_DATA, SERVICE_LINKS } from "./constants";
 import Link from "next/link";
 import { getTranslations } from "next-intl/server";
+import FooterMap from "./footerMap";
 
 export default async function Footer() {
     const t = await getTranslations();
@@ -47,15 +48,8 @@ export default async function Footer() {
                             );
                         })}
                     </div>
-                    <div className="flex flex-col ga4">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d463877.31242483313!2d46.492886205368556!3d24.725455373700168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2sRiyadh%20Saudi%20Arabia!5e0!3m2!1sen!2str!4v1766437131415!5m2!1sen!2str"
-                            width="600"
-                            height="450"
-                            loading="lazy"
-                            title="footer google maps"
-                            className="w-full h-63 rounded-xl invert-95 hue-rotate-10 contrast-120 saturate-160"
-                        ></iframe>
+                    <div className="flex flex-col">
+                        <FooterMap />
                     </div>
                 </div>
                 <p className="text-xl grid tracking-tighter grid-span-2">
