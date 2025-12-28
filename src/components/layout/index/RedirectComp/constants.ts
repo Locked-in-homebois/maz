4e5891bad36e3da1a4c80fd064ec54d496d1d837
Parsa <parsaxavier@gmail.com>
2025-12-28 18:30:54 +0300

    redirect carousel init

--- src/components/layout/index/RedirectComp/constants.ts
+++ src/components/layout/index/RedirectComp/constants.ts
@@ -1,22 +1,27 @@
 import { RedirectComp } from "./types";
 
 export const REDIRECTCONTENT: RedirectComp[] = [
-    {
-        title: "RedirectComp.title.L",
-        description: "RedirectComp.description.L",
+	{
+		title: "RedirectComp.title.L",
+		description: "RedirectComp.description.L",
 
-        buttontext: "RedirectComp.buttonText.L",
+		buttontext: "RedirectComp.buttonText.L",
 
-        image: { src: "/BabaImgHero.jpeg", alt: "baba index page" },
-        href: "/aboutme",
-    },
-    {
-        title: "RedirectComp.title.R",
-        description: "RedirectComp.description.R",
+		image: { src: "/BabaImgHero.jpeg", alt: "baba index page" },
+		href: "/aboutme",
+	},
+	{
+		title: "RedirectComp.title.R",
+		description: "RedirectComp.description.R",
 
-        buttontext: "RedirectComp.buttonText.R",
+		buttontext: "RedirectComp.buttonText.R",
 
-        image: { src: "/reno2v5.jpeg", alt: "villa interior image" },
-        href: "/projects",
-    },
+		image: { src: "/reno2v5.jpeg", alt: "villa interior image" },
+		slides: [
+			{ src: "", alt: "" },
+			{ src: "", alt: "" },
+			{ src: "", alt: "" },
+		],
+		href: "/projects",
+	},
 ];
